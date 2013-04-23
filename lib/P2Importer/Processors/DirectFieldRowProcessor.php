f0fd7f79b6aa06cbad605ac4aec158bb32be63a6
Ray Stuart <rstuart@phase2technology.com>
2013-04-23 15:54:28 +0000

    Update DirectFieldRowProcessor.php to account for revisions properly and start using db_merge.
    Add drush_log messages to help determine what is happening, including warning
    when an nid to update is not found.  Replace drupal_write_record with db_merge
    as best way to handle cases where field content did not exist in a particular
    revision. Add notes regarding where multiple value fields need to be accounted for.

--- lib/P2Importer/Processors/DirectFieldRowProcessor.php
+++ lib/P2Importer/Processors/DirectFieldRowProcessor.php
@@ -17,8 +17,10 @@ class DirectFieldRowProcessor extends NodeRowProcessor {
    * @return ProcessorInterface
    */
   public function process(DataContainer $data, \Pimple $registry) {
+
     // Get the nid of the node
     if ($nid = $this->node_exists($data, $registry)) {
+
       // Get the vids that need to be updated
       $vids = db_select('node_revision_states', 'nrs')
         ->fields('nrs', array('vid'))
@@ -29,36 +31,100 @@ class DirectFieldRowProcessor extends NodeRowProcessor {
 
       $vids ?: array();
 
-      // Update fields
+      // Get the current revision id for the node.
+      $current_vid = db_select('node', 'n')
+        ->fields('n', array('vid'))
+        ->condition('nid', $nid)
+        ->execute()
+        ->fetchField();
+
+      // Manually update fields across revisions.
       foreach($vids as $vid) {
-        // because we are only updating taxonomy and text field
+
+        // Log message to terminal.
+        if (function_exists('drush_log')) {
+          drush_log(dt('Syncing fields on node !nid, revision !vid (Current revision is !currentvid)',
+            array('!nid' => $nid, '!vid' => $vid, '!currentvid' => $current_vid)), 'ok');
+        }
+
+        // Load specific node revision to check existence of field values.
+        // This may not be necessary anymore since we are now using db_merge,
+        // but there may be additional future benefits, so leaving in place.
+        $node = node_load($nid, $vid);
+
+        // Because we are only updating taxonomy and text field
         // This will only work with a DB backed field storage
         // This would not work with field field
         foreach ($data as $field_name => $field_value) {
           $field_info = field_info_field($field_name);
+          // @TODO: This is only affecting the first item.  Need to update this
+          //   to work with fields with multiple values.
           $value = $field_value[LANGUAGE_NONE][0];
-
-          $transaction = db_transaction();
+          if (!$value) {
+            error_log($field_name);
+            watchdog('psu_ps_client', "fieldname is @name", array('@name' => $field_name));
+          }
 
           try {
             $revision_table = _field_sql_storage_revision_tablename($field_info);
             $table = _field_sql_storage_tablename($field_info);
-            $condition_fields = array();
+            $fields = array();
             foreach($value as $key => $real_value) {
-              $condition_fields[_field_sql_storage_columnname($field_name, $key)] =
-                $real_value;
+              if ($key != 'safe_value') {
+                $fields[_field_sql_storage_columnname($field_name, $key)] =
+                  $real_value;
+              }
             }
 
-            $condition_fields['revision_id'] = $vid;
-            drupal_write_record($revision_table, $condition_fields, array('revision_id'));
-            drupal_write_record($table, $condition_fields, array('revision_id'));
+            // Set the field values.
+            $fields['entity_type'] = 'node';
+            $fields['bundle'] = $node->type;
+            $fields['language'] = $node->language;
+            $fields['deleted'] = 0;
+            $fields['delta'] = 0;
+            $fields['entity_id'] = $nid;
+
+            $fields['revision_id'] = $vid;
+
+            // Write the revisions table for all items.
+            db_merge($revision_table)
+              ->key(array(
+                  'entity_type' => $fields['entity_type'],
+                  'entity_id' => $fields['entity_id'],
+                  'revision_id' => $fields['revision_id'],
+                  'deleted' => $fields['deleted'],
+                  'delta' => $fields['delta'],
+                  'language' => $fields['language'],
+              ))
+              ->fields($fields)
+              ->execute();
+
+
+            // Write to the data table only if this is for the current version.
+            if ($vid == $current_vid) {
+              db_merge($table)
+                ->key(array(
+                    'entity_type' => $fields['entity_type'],
+                    'entity_id' => $fields['entity_id'],
+                    'deleted' => $fields['deleted'],
+                    'delta' => $fields['delta'],
+                    'language' => $fields['language'],
+                ))
+                ->fields($fields)
+                ->execute();
+            }
           }
           catch (\Exception $e) {
-            $transaction->rollback();
             watchdog_exception('import', $e);
             throw $e;
           }
         }
+      }
+    }
+    else {
+      if (function_exists('drush_log')) {
+        drush_log(dt('P2-Importer: node_exists() did not return an nid. Reference nid is !data.',
+          array('!data' => $data->getOriginalValue('nid'))), 'warning');
       }
     }
   }
